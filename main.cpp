da8117e59c62a4b23dcc0970d793564179630b8c
Yuji_Itadorii <rawat141abhay@gmail.com>
2022-04-05 09:39:28 +0530

    Code of Login part

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ bool usernameValidator(string s)
     int count = 0;
     for (int i = 0; i < s.size(); i++)
     {
-        if ((int)s[i]==32)
+        if ((int)s[i] == 32)
         {
             return true;
         }
@@ -39,13 +39,13 @@ int main()
 {
     cout << "Enter your choice : \n 1.Resgistration \n 2.Login\n\n";
     string choice;
-    getline(cin,choice);
-    if (choice[0]=='1' )
+    getline(cin, choice);
+    if (choice[0] == '1')
     {
         string username, password;
         cout << "Rules of Username : \n1.It must not have any special charator\n2.It must not have any space between the name\n";
         cout << "Enter your username:- \n";
-        getline(cin,username);
+        getline(cin, username);
         if (usernameValidator(username))
         {
             cout << "Invalid Username entered!!\n";
@@ -54,7 +54,7 @@ int main()
 
         cout << "Rules of Password : \n1.It must not be more than 8 characters\n2.It should have special characters\n";
         cout << "\nEnter your password\n";
-        getline(cin,password);
+        getline(cin, password);
         if (passwordValidator(password))
         {
             cout << "Password length more than 8 characters!!\n";
@@ -66,9 +66,36 @@ int main()
         file << username << endl
              << password;
         file.close();
+        cout << "Congratulation!! your are registered now!!\n";
     }
     else
     {
+        string username, password;
+        cout << "Enter your username:- \n";
+        getline(cin, username);
+        ifstream file;
+        file.open(username + ".txt");
+        if (file)
+        {
+            cout << "\nEnter your password\n";
+            getline(cin, password);
+
+            string dumyline, check_password;
+            getline(file, dumyline);
+            getline(file, check_password);
+            if (password.compare(check_password))
+            {
+                cout<<"You have entered Wrong Password!!";
+            }
+            else{
+                cout << "You are logged in !!\n";
+            }
+        }
+        else
+        {
+            cout << "Username does not exsist!! Please Register Your username and Passsword\n";
+            return 0;
+        }
     }
     return 0;
 }
